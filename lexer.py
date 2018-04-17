e478ce102f0faa401926959f5c226bf006de0359
Gavin Goodship <msgoodship1@gmail.com>
2018-04-17 22:42:06 +0100

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,5 +1,7 @@
+import ply.lex
+
 
 class Lexer(object):
-  def __init__(self, pos,program):
-    self.pos = pos
+  def __init__(self, program):
     self.program = program
+    tokens = ["name","plus","minus","if","else","lessthan"]
