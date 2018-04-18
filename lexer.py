740ad6bc8469a754fc9ca7efd62f4679f9a0076f
goodship1 <msgoodship1@gmail.com>
2018-04-18 14:31:41 +0100

    adding tokens

--- lexer.py
+++ lexer.py
@@ -1,7 +1,21 @@
-import ply.lex
+import ply.lex as lex
 
 
 class Lexer(object):
+
   def __init__(self, program):
     self.program = program
-    tokens = ["name","plus","minus","if","else","lessthan"]
+
+  tokens = ("name","plus","minus","lessthan","equals",
+            "if","else","leftpara","rightpara","openbrace","closingbrace"
+            )
+
+  t_plus = r'\+'
+  t_minus = r'-'
+  t_lessthan = r'<'
+  t_equals = r'='
+  t_greaterthan = r'>'
+  t_ignore = r'\t'#ignore white spaces
+  t_name = r'[a-zA-Z_][a-zA-Z0-9_]*'#names must start with a letter and not a number
+
+  reserved = dict()
