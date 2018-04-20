15f7210617488d46fedf9c8aa662cdb439d469cf
goodship1 <msgoodship1@gmail.com>
2018-04-20 15:32:21 +0100

    rewriting lexer made mistake of defining everything again and got rid of class for lexer

--- lexer.py
+++ lexer.py
@@ -1,87 +1,52 @@
 import ply.lex as lex
 import re
 
-class Lexer(object):
 
-  def __init__(self, program):
-    self.program = program
+"""tuple of tokens for tokenzing"""
 
-  tokens = ("name","plus","minus","lessthan","equals",
+
+tokens = ("name","plus","minus","lessthan","equals",
             "if","else","openingpara","closingpara","openbrace","closingbrace",
-            "string","number"
+            "string","number","colon","greaterthan"
             )
 
-  t_plus = r'\+'
-  t_minus = r'-'
-  t_colon = r';'
-  t_lessthan = r'<'
-  t_equals = r'='
-  t_greaterthan = r'>'
-  t_leftpara = r'('
-  t_rightpara = r')'
-  t_openbrace = r'{'
-  t_closingbrace = r'}'
-  t_ignore = r'\t'#ignore white spaces
-  t_name = r'[a-zA-Z_][a-zA-Z0-9_]*'#names must start with a letter and not a number
 
-  reserved = {"print":"PRINT","if":"IF","else","ELSE"}#reserved key words
+"""simple tokens"""
+t_plus = r'\+'
+t_minus = r'-'
+t_colon = r';'
+t_lessthan = r'<'
+t_equals = r'='
+t_greaterthan = r'\>'
+t_openingpara = r'\('
+t_closingpara = r'\)'
+t_openbrace = r'{'
+t_closingbrace = r'}'
+t_ignore = r't'#ignore white spaces
+t_name = r'[a-zA-Z_][a-zA-Z0-9_]*'#names must start with a letter and not a number
 
-  def t_name(self,t):
-      """tokenzies names"""
-     r'[a-zA-Z_][a-zA-Z0-9_]*'
-     return t
+reserved = {"print":"PRINT","if":"IF","else":"ELSE"}#reserved key words
 
-  def t_closingbrace(self,t):
-      r'\}'
-      return t
-
-  def t_openbrace(self,t):
-      r'{'
-      return t
-
-  def t_leftpara(self,t):
-      r'('
-      return t
-
-  def t_rightpara(self,t):
-      r'\)'
-      return t
-
-  def t_equals(self,t):
-      r'='
-      return t
-
-  def t_greaterthan(self,t):
-      r'>'
-      return t
-
-  def t_plus(self,t):
-      r'+'
-      return t
-
-  def t_string(self,t):
-      r'(\".*\"|\'.*\')'
-      t.value = t.value[1:-1]
-      return t
-
-  def t_number(self,t):
-      r'\d+'
-      t.value = int(t.value)
-      return t
-
-  def t_newline(self,t):
-      r'\n+'
-      t.lexer.lineno += len(t.value)
-      return t
-
-  def t_error(self,t):
-      print("illegal declaration %s"%t.value[0])
-      t.lexer.skip(1)
+"""more advance rules for token"""
 
 
+def t_number(t):
+    """token for ints"""
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_string(t):
+    """token for string"""
+    r'(\".*\"|\'.*\')'
+    t.value = t.value[1:-1]
+    return t
 
 
-  def run_Lexer(self):
+def t_error(t):
+    """error handling of lexer"""
+    print("this is an error %s"%t.value[0])
+    t.skip(1)
 
-      "used to run lexer"""
-      pass
+
+lex.lex()
