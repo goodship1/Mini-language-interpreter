5420ed467412dc85e35b279245d224fcf841664b
goodship1 <msgoodship1@gmail.com>
2018-04-30 23:26:24 +0100

    started to writer parser.py

--- parser.py
+++ parser.py
@@ -1,3 +1,14 @@
 
 from lexer import tokens
 from ply.yacc import yacc
+
+
+def p_expression_equals(p):
+    pass
+
+def p_expression_lessthan(p):
+    'expression : expression LESSTHAN term'
+    p[0] = p[1] < p[3]
+
+def p_expresion_plus(p):
+    pass
