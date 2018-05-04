633f88b7f40053af6bf57e41011babc9270d2624
goodship1 <msgoodship1@gmail.com>
2018-05-04 14:46:36 +0100

    started to write parser.py

--- parser.py
+++ parser.py
@@ -5,11 +5,16 @@ from ply.yacc import yacc
 
 def p_assign(p):
     '''assign : ID EQUALS expression'''
-    pass
+    p[0] = p[1]
 
 
 def p_expression_lessthan(p):
     pass
+
+def p_assign_number(p):
+    '''assign : ID EQUALS factor'''
+    p[0] = p[1]
+
 
 
 yacc()
