f069d88f6d35c5ae14c71bc0182b06c6e51c1941
goodship1 <msgoodship1@gmail.com>
2018-05-15 21:30:41 +0100

    rewriting parser.py

--- parser.py
+++ parser.py
@@ -5,9 +5,25 @@ from ply.yacc import yacc
 
 def p_assign(p):
     '''assign : ID EQUALS expression COLON'''
-    pass
+    p[0] = p[1]
 
 def p_expression(p):
+    '''expression: ID LESSTHAN term'''
+    p[0] = p[1] < p[2]
+
+def p_if_statement(p):
+    pass
+
+def p_else_statement(p):
+    pass
+
+def p_expression_number(p):
+    pass
+
+def p_expression_ID(p):
+    pass
+
+def p_print_statement(p):
     pass
 
 def p_statement(p):
