b7313738268c2cb7c33eb889e99e03dba8d5ffad
Gavin Goodship <msgoodship1@gmail.com>
2018-05-15 22:24:06 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -2,14 +2,18 @@
 from lexer import tokens
 from ply.yacc import yacc
 
+names = dict()#stores names data
+
+precednece = tuple()
+
 
 def p_assign(p):
     '''assign : ID EQUALS expression COLON'''
     p[0] = p[1]
 
-def p_expression(p):
-    '''expression: ID LESSTHAN term'''
-    p[0] = p[1] < p[2]
+def p_expression_lessthan(p):
+    '''expression: ID LESSTHAN NUMBER'''
+    p[0] = p[1] < p[3]
 
 def p_if_statement(p):
     pass
@@ -17,8 +21,13 @@ def p_if_statement(p):
 def p_else_statement(p):
     pass
 
-def p_expression_number(p):
+def p_group_expression(p):
     pass
+
+def p_expression_number(p):
+    '''expression : NUMBER'''
+    p[0] = p[1]
+
 
 def p_expression_ID(p):
     pass
