de23bc7b75b7b474513fea3d8c064a4a31f93f15
goodship1 <msgoodship1@gmail.com>
2018-07-01 14:57:05 +0100

    started to write test to parsertest.py

---
+++ tests/parsertest.py
@@ -0,0 +1,91 @@
+import pytest
+
+
+from lexer import tokens
+from ply.yacc import yacc
+
+
+
+def p_assign_factor(p):
+    '''assign : ID EQUALS factor COLON'''
+
+    p[0] = ('assignment-factor',p[3])
+
+def p_assign_string(p):
+    '''assign : ID EQUALS STRING COLON'''
+    p[0] = ("assign-string",p[3])
+
+
+def p_lessthan(p):
+    '''expression : ID LESSTHAN factor'''
+    p[0] = ("lessthan-expression",p[3])
+
+def p_if_statement(p):
+    '''if : IF OPENINGPARA ID LESSTHAN factor CLOSINGPARA OPENBRACE PRINT STRING COLON CLOSINGBRACE ELSE OPENBRACE PRINT ID COLON CLOSINGBRACE'''
+    p[0] = ("if-else-statement",p[5])
+
+
+def p_expression_group(p):
+    '''expression : OPENINGPARA expression CLOSINGPARA'''
+    p[0] = ("group-expression", p[2])
+
+
+def p_group_lessthan(p):
+    '''expression : OPENINGPARA ID LESSTHAN factor CLOSINGPARA'''
+    p[0] =("group-lessthan-factor",p[4])
+
+
+def p_factor_number(p):
+    '''factor : NUMBER'''
+    p[0] = ("number-factor",p[1])
+
+
+
+def p_expression_ID(p):
+    ''' expression : ID'''
+    p[0] = ("id-expression",p[1])
+
+def p_expression_string(p):
+    '''expression : STRING'''
+    p[0]  = ("string-expression",p[1])
+
+def p_print(p):
+    '''expression : PRINT STRING COLON
+                 | PRINT ID COLON'''
+    if(p[2]== "STRING"):
+        p[0] = ("print-STRING",p[2])
+    elif(p[2]== "ID"):
+        p[0] =("print-ID",p[2])
+
+
+def p_error(p):
+    print("parser error")
+
+
+def factor_Assign_test():
+    pass
+
+
+def string_Assign_test():
+    pass
+
+def number_Test():
+    pass
+
+
+def string_Test():
+    pass
+
+
+def error_Test():
+    pass
+
+
+def lessthan_Factor_test():
+    pass
+
+
+def group_Test():	
+    pass
+
+ 
