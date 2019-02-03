4913d1a6d16ebf4f1c4c7a6ce23cb344e6050918
Gavin Goodship <msgoodship1@gmail.com>
2019-02-03 23:09:12 +0000

    Update AST.py
    started to write the ast for the grammer

--- AST.py
+++ AST.py
@@ -1,1 +1,22 @@
 
+class Number(object):
+    
+    def __init__(self,number):
+      self.number = number
+ 
+
+
+class BinaryOperation(object):
+   
+    def __init__(self,left , right ,operation):
+      self.left = left
+      self.right = right
+      self.opeartion = operation 
+    
+    
+class expression(object):
+  
+  def __init__(self,expression):
+    self.expression = expression
+    
+    
