5a473ad9ed33f2bb6ad65a4c3e78d9309149bc06
Gavin Goodship <msgoodship1@gmail.com>
2019-03-04 14:27:14 +0000

    Adding test statements to parsertest.py

--- tests/parsertest.py
+++ tests/parsertest.py
@@ -2,18 +2,31 @@ import pytest
 from lexer import tokens
 from ply.yacc import yacc
 
+global parser = yacc()
 
 def assignment_Number():
-    '''testing assignment of number'''
-    parser = yacc()
-    assignment_Number = parser.parse('x=10;')
+    '''testing assignment of number test'''
+    assignment_Number = global parser.parse('x=10;')
     assert(assignment_Number) == "(assignment,'x','=','10')"  
 
 def assignment_String():
-    '''test assignmend of string'''
-    parser = yacc()
-    assignment_String = parser.parse("x='Test';")
+    '''assignment x = 'Test'; test'''
+    assignment_String = global parser.parse("x='Test';")
     assert(assignment_String) == "(assignment,'x','=',''Test'')
     
     
- 
+ def add_Expression():
+    '''number + number colon test'''
+    addation_Numbers = global parser.parse("1+1;")
+    pass
+
+def minus_Expression():
+    ''' number - number colon  test'''
+     subtract_Number = global parse.parse('10 - 1;')
+     pass
+
+
+def times_Expression():
+    '''number * number colon test'''
+    multiplication = global parse.parse("10*10;")
+    pass
