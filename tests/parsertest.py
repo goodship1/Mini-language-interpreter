0b70be305ffa9cfe0f76991c0f262ae22fc4cbab
Gavin Goodship <msgoodship1@gmail.com>
2019-03-05 14:55:50 +0000

    adding lessthan number test

--- tests/parsertest.py
+++ tests/parsertest.py
@@ -12,21 +12,37 @@ def assignment_Number():
 def assignment_String():
     '''assignment x = 'Test'; test'''
     assignment_String = global parser.parse("x='Test';")
-    assert(assignment_String) == "(assignment,'x','=',''Test'')
+    assert(assignment_String) == "(assignment,'x','= ',' Test')"
     
     
  def add_Expression():
     '''number + number colon test'''
     addation_Numbers = global parser.parse("1+1;")
-    pass
+    assert(addation_Numbers) == "('add','+','1','1')"
+    
 
 def minus_Expression():
     ''' number - number colon  test'''
      subtract_Number = global parse.parse('10 - 1;')
-     pass
+     assert(subtract_Number) == "('minius','-','10','1')"
 
 
+    
+    
 def times_Expression():
     '''number * number colon test'''
     multiplication = global parse.parse("10*10;")
-    pass
+    assert(multiplication) == "('times','*','10','10')"
+    
+    
+ def lessthan_Expression():
+    '''number < number colon test'''
+    lessthan = global parse.parse("1 < 10;")
+    assert(lessthan) == "('lessthan' , '<' ,'1','10')"
+
+    
+ def greaterthan_Expression():
+    '''number > number colon test'''
+    greaterthan = global parse.parse("100 > 20;")
+    pass    
+            
