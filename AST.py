535fd072a8b783fdc0ec76399a1bd756f13c1810
Gavin Goodship <msgoodship1@gmail.com>
2019-04-12 16:59:45 +0100

    working on the AST
    writing the ast nodes

--- AST.py
+++ AST.py
@@ -1,26 +1,62 @@
-class Node(object):
-    pass
-
-
+class String(object):
+    def __init__(self,string):
+        self.string = string
+   
+    def evalutation(self):
+        return self.string
+    
 
 class Number(object):
     
     def __init__(self,number):
       self.number = number
+    
+    def evaluation(self):
+        return self.number
  
 
-
-class BinaryOperation(object):
+class Times(object):
+    def __init__(self,left,right):
+        self.right = right
+        self.left = left
+    
+    def evaluation(self):
+       return  self.left.evaluation() * self.right.evaluation()
+        
+class Lessthan(object):
+    def __init__(self,left,right):
+        self.right = right
+        self.left = left
+    
+    def evaluation(self):
+        return self.left.evaluation() < self.right.evaluation()
+    
+    
+    
+class Greaterthan(object):
+    def __init__(self,left,right):
+        self.right = right
+        self.left = left
    
-    def __init__(self,left , right ,operation):
-      self.left = left
-      self.right = right
-      self.opeartion = operation 
+    def evaluation(self):
+        return self.left.evaluation() > self.right.evaluation()
+
+
+
+class Subtraction(object):
+    def __init__(self,left,right):
+        self.left = left
+        self.right = right
     
+    def evaluation(self):
+        return self.left.evalutation() - self.right.evaluation()
+
+class Addation(object):
+    def __init__(self,right,left):
+        self.left = left
+        self.right = right
+        
+    def evaluation(self):
+        return self.left.evaluation() +  self.right().evaluation()
     
-class expression(object):
-  
-  def __init__(self,expression):
-    self.expression = expression
-    
-    
+
